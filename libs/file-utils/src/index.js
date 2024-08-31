a312bd9c250348042eeaf972672040737a47264f
HormCodes <horm@horm.codes>
2024-08-31 23:21:28 +0200

    fix: slides subpage without html suffix

--- libs/file-utils/src/index.js
+++ libs/file-utils/src/index.js
@@ -5,10 +5,40 @@ module.exports.writeToFile = function ({
   fileName = 'output',
   content = 'No output provided.',
 }) {
-  try {
-    mkdirSync(directory);
-  } catch (e) {}
-  try {
-    writeFileSync(`${directory}/${fileName}`, content);
-  } catch (e) {}
+  createDirIfNotExist(directory);
+  createFileIfNotExist(`${directory}/${fileName}`, content);
 };
+
+function createDirIfNotExist(name = '.') {
+  if (name === '.' || name === '..') {
+    return;
+  }
+
+  if (!name.includes('/')) {
+    try {
+      mkdirSync(name);
+    } catch (e) {}
+    return;
+  }
+
+  const subDirectories = name.split('/').reduce((result, subDirectory) => {
+    if (result.length === 0) {
+      return [subDirectory];
+    }
+
+    const lastSubDirectory = result[result.length - 1];
+    return [...result, `${lastSubDirectory}/${subDirectory}`];
+  }, []);
+
+  subDirectories.forEach((subDirectory) => {
+    try {
+      mkdirSync(subDirectory);
+    } catch (e) {}
+  });
+}
+
+function createFileIfNotExist(fileName = '', content = '') {
+  try {
+    writeFileSync(fileName, content);
+  } catch (e) {}
+}
